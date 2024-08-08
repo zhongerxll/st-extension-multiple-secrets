cd39894c8fb7943d90525864c0cb34cc18eb0445
zhongerxll <zhongerxll@gmail.com>
2024-08-08 15:39:01 -0500

    1.0.0

--- index.js
+++ index.js
@@ -1,63 +1,88 @@
-// The main script for the extension
-// The following are examples of some basic extension functionality
-
-//You'll likely need to import extension_settings, getContext, and loadExtensionSettings from extensions.js
 import { extension_settings, getContext, loadExtensionSettings } from "../../../extensions.js";
+import { eventSource, event_types, saveSettingsDebounced } from "../../../../script.js";
+import { getRequestHeaders, callPopup } from '../../../../script.js';
+import { oai_settings } from '../../../openai.js';
+import { secret_state, updateSecretDisplay, writeSecret } from '../../../secrets.js';
 
-//You'll likely need to import some other functions from the main script
-import { saveSettingsDebounced } from "../../../../script.js";
-
-// Keep track of where your extension is located, name should match repo name
-const extensionName = "st-extension-example";
+const extensionName = "st-extension-multiple-secrets";
 const extensionFolderPath = `scripts/extensions/third-party/${extensionName}`;
 const extensionSettings = extension_settings[extensionName];
 const defaultSettings = {};
 
-
- 
-// Loads the extension settings if they exist, otherwise initializes them to the defaults.
-async function loadSettings() {
-  //Create the settings if they don't exist
-  extension_settings[extensionName] = extension_settings[extensionName] || {};
-  if (Object.keys(extension_settings[extensionName]).length === 0) {
-    Object.assign(extension_settings[extensionName], defaultSettings);
-  }
-
-  // Updating settings in the UI
-  $("#example_setting").prop("checked", extension_settings[extensionName].example_setting).trigger("input");
+async function switchSecretsFromArray(generationType, _args, isDryRun) {
+    if (!isDryRun) {
+        const key = "api_key_" + oai_settings.chat_completion_source;
+        try {
+            const response = await fetch('/api/plugins/multiple-secrets/switch', {
+                method: 'POST',
+                headers: getRequestHeaders(),
+                body: JSON.stringify({
+                    key
+                }),
+            });
+            if (response.status == 200) {
+                secret_state[key] = true;
+                updateSecretDisplay();
+            }
+        } catch {
+            console.error('Could not write secret value: ', key);
+        }
+    }
 }
 
-// This function is called when the extension settings are changed in the UI
-function onExampleInput(event) {
-  const value = Boolean($(event.target).prop("checked"));
-  extension_settings[extensionName].example_setting = value;
-  saveSettingsDebounced();
+async function saveSecrets() {
+    const key = "api_key_" + oai_settings.chat_completion_source
+    const value = Array.from(document.querySelectorAll('.multiple-secrets-field')).map(input => input.value).filter(item => item.length > 0);
+    await writeSecret(key + "_array", value)
+    await switchSecretsFromArray()
+    $('#api_button_openai').trigger('click');
 }
 
-// This function is called when the button is clicked
-function onButtonClick() {
-  // You can do whatever you want here
-  // Let's make a popup appear with the checked setting
-  toastr.info(
-    `The checkbox is ${extension_settings[extensionName].example_setting ? "checked" : "not checked"}`,
-    "A popup appeared because you clicked the button!"
-  );
+async function addMultipleSecretsField() {
+    $('#multiple-secrets-container').append(`
+        <div class="flex-container">
+            <input class="text_pole flex1 multiple-secrets-field" maxlength="500" value="" type="text" autocomplete="off" data-i18n="[placeholder]Please enter your API key" placeholder="Please enter your API key"/>
+            <div title="Clear your API key" data-i18n="[title]Clear your API key" class="menu_button fa-solid fa-circle-xmark remove-multiple-secrets-field"></div>
+        </div>
+	`);
 }
 
-// This function is called when the extension is loaded
-jQuery(async () => {
-  // This is an example of loading HTML from a file
-  const settingsHtml = await $.get(`${extensionFolderPath}/example.html`);
+async function viewSecrets() {
+    const response = await fetch('/api/secrets/view', {
+        method: 'POST',
+        headers: getRequestHeaders(),
+    });
 
-  // Append settingsHtml to extensions_settings
-  // extension_settings and extensions_settings2 are the left and right columns of the settings menu
-  // Left should be extensions that deal with system functions and right should be visual/UI related 
-  $("#extensions_settings").append(settingsHtml);
+    if (response.status == 403) {
+        callPopup('<h3>Forbidden</h3><p>To view your API keys here, set the value of allowKeysExposure to true in config.yaml file and restart the SillyTavern server.</p>', 'text');
+        return;
+    }
 
-  // These are examples of listening for events
-  $("#my_button").on("click", onButtonClick);
-  $("#example_setting").on("input", onExampleInput);
+    if (!response.ok) {
+        return;
+    }
 
-  // Load settings when starting things up (if you have any)
-  loadSettings();
+    $('#dialogue_popup').addClass('wide_dialogue_popup');
+    const data = await response.json();
+    const table = document.createElement('table');
+    table.classList.add('responsiveTable');
+    $(table).append('<thead><th>Key</th><th>Value</th></thead>');
+
+    for (const[key, value] of Object.entries(data)) {
+        $(table).append(`<tr><td>${DOMPurify.sanitize(key)}</td><td>${DOMPurify.sanitize(value)}</td></tr>`);
+    }
+
+    callPopup(table.outerHTML, 'text');
+}
+
+jQuery(async() => {
+    const settingsHtml = await $.get(`${extensionFolderPath}/settings.html`);
+    $("#extensions_settings").append(settingsHtml);
+    $("#save-secrets").on("click", saveSecrets);
+    $('#view-secrets').on('click', viewSecrets);
+    $("#add-multiple-secrets-field").on("click", addMultipleSecretsField);
+    $(document).on('click', '.remove-multiple-secrets-field', function() {
+        $(event.target).closest('.flex-container').remove();
+    });
+    eventSource.on(event_types.GENERATION_STARTED, switchSecretsFromArray);
 });
